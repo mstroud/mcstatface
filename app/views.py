133949003fe5a84fb5742fe5cbdf9c791ad5696b
mstroud <matt.c.stroud@gmail.com>
2018-08-01 13:02:31 -0500

    Added barebones object dump to main view

--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
 from flask import render_template
 from app import app
 
+from mcstatus import MinecraftServer
+
+def dump(obj):
+   str = ''
+   for attr in dir(obj):
+       if hasattr( obj, attr ):
+           str = str + "obj.%s = %s\n" % (attr, getattr(obj, attr))
+   return str
+
 @app.route('/')
 def index():
+    server = MinecraftServer.lookup("alsochris.com:25565")
+    status = server.status()
     return render_template("index.html",
-        title="Simple Flask Template")
+        title="alsochris.com Minecraft Status",
+        server=dump(server),
+        status=dump(status))
