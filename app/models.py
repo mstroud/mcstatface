8bc7b1eac1282ef5c1ed691519f2b2c6bc5e9fa3
mstroud <matt.c.stroud@gmail.com>
2018-08-01 15:58:26 -0500

    Cleaned up templates, model class data made non-static

--- app/models.py
+++ app/models.py
@@ -3,6 +3,10 @@ from app import app
 from mcstatus import MinecraftServer
 
 class StatusModel(object):
-    server = MinecraftServer.lookup(app.config['MCSTATUS_URL'])
-    status = server.status()
+    def __init__(self,urlstr=None):
+        if urlstr:
+            self.server = MinecraftServer.lookup(urlstr)
+        else:
+            self.server = MinecraftServer.lookup(app.config['MCSTATUS_URL'])
+        self.status = self.server.status()
     
--- app/views.py
+++ app/views.py
@@ -5,6 +5,4 @@ from app import models
 @app.route('/')
 def index():
     s = models.StatusModel()
-    return render_template("index.html",
-        title="alsochris.com Minecraft Status",
-        s=s)
+    return render_template("index.html",s=s)
